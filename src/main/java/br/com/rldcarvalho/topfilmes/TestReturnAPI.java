b25a9051b55c3e05ade0a90e8314b310117cbfb8
Rafael <rldcarvalhoo@gmail.com>
2022-08-11 22:34:31 -0300

    Split movies array in datas arrays

--- src/main/java/br/com/rldcarvalho/topfilmes/TestReturnAPI.java
+++ src/main/java/br/com/rldcarvalho/topfilmes/TestReturnAPI.java
@@ -6,6 +6,10 @@ import java.net.*;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class TestReturnAPI {
     static String apiKey = "enter with api key";
@@ -19,14 +23,84 @@ public class TestReturnAPI {
                 .newBuilder()
                 .uri(new URI(apiPath + apiKey))
                 .GET().build();
+//        Requisitar e imprimir diretamente:
+//        client.sendAsync(request, HttpResponse.BodyHandlers.ofString())
+//                .thenApply(HttpResponse::body)
+//                .thenAccept(System.out::println)
+//                .join();
 
-        client.sendAsync(request, HttpResponse.BodyHandlers.ofString())
-                .thenApply(HttpResponse::body)
-                .thenAccept(System.out::println)
-                .join();
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+        String json = response.body();
+
+        String jsonSubstring = extractStringBetweenSquareBracket(json);
+
+        String [] moviesArray = jsonSubstring.split("(?<=}),");
+
+        System.out.println(moviesArray[0]);
+
+        List<String> titles = parseTitle(moviesArray);
+
+        List<String> urlImages = parseUrlImages(moviesArray);
+
+        List<String> years = parseYears(moviesArray);
+
+        List<String> imdbRatings = parseImdbRatings(moviesArray);
+
+        titles.forEach(System.out::println);
 
 
+    }
 
+
+    public static List<String> parseTitle(String[] moviesArray){
+        List<String> titles = new ArrayList<>();
+        for (String movie: moviesArray) {
+            int indexOfStartTitle = movie.indexOf("\"title\":") + 9;
+            int indexOfEndTitle = movie.indexOf("\",\"fullTitle");
+            titles.add(movie.substring(indexOfStartTitle, indexOfEndTitle));
+        }
+        return titles;
+    }
+
+    public static List<String> parseUrlImages(String[] moviesArray){
+        List<String> urlImages = new ArrayList<>();
+        for (String movie: moviesArray) {
+            int indexOfStartUrl = movie.indexOf("\"image\":") + 9;
+            int indexOfEndUrl = movie.indexOf("\",\"crew");
+            urlImages.add(movie.substring(indexOfStartUrl, indexOfEndUrl));
+        }
+        return urlImages;
+    }
+
+    public static List<String> parseImdbRatings(String[] moviesArray){
+        List<String> imdbRatings = new ArrayList<>();
+        for (String movie: moviesArray) {
+            int indexOfStartImdbRating = movie.indexOf("\"imDbRating\":") + 14;
+            int indexOfEndImdbRating = movie.indexOf("\",\"imDbRatingCount");
+            imdbRatings.add(movie.substring(indexOfStartImdbRating, indexOfEndImdbRating));
+        }
+        return imdbRatings;
+    }
+
+    public static List<String> parseYears(String[] moviesArray){
+        List<String> years = new ArrayList<>();
+        for (String movie: moviesArray) {
+            int indexOfStartYear = movie.indexOf("\"year\":") + 8;
+            int indexOfEndYear = movie.indexOf("\",\"image");
+            years.add(movie.substring(indexOfStartYear, indexOfEndYear));
+        }
+        return years;
+    }
+    public static String extractStringBetweenSquareBracket(String json){
+        int indexOfFirstSquareBracket= json.indexOf("[");
+        int indexOfLastSquareBracket= json.indexOf("]");
+        String jsonSubstring = json.substring(indexOfFirstSquareBracket + 1 , indexOfLastSquareBracket);
+        return jsonSubstring;
+    }
+    public List<String> extract(String json, String regex){
+
+        return null;
     }
 
     public static String getImdbTop250MovieString() throws Exception {
